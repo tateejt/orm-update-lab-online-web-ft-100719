9069f405527a7210cd56feed1a0781dab1663c9f
Emery Tate <tateejt@gmail.com>
2019-11-15 03:30:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,7 +37,7 @@ def save
       VALUES (?, ?)
     SQL
  
-    DB[:conn].execute(sql, self.name, self.album)
+    DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
   end
 
