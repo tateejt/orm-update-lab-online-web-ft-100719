10147764a86a1d949a41eb250b50c1843b3a47c9
Emery Tate <tateejt@gmail.com>
2019-11-15 03:34:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,7 +47,10 @@ end
 
 
 def update
-  
+    def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 end
 
 end
