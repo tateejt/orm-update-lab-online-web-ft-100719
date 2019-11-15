b0e52bba0d649fa8c83be9c0750567e77b12e9d6
Emery Tate <tateejt@gmail.com>
2019-11-15 03:21:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,8 +5,12 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
   
+  attr_accessor :name, :id, :grade
+  
   def initialize(id=nil, name, grade)
-    
+    @id = id
+    @name = name
+    @grade = grade
   end
 
 
